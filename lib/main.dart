9e8f8d5034b46b9db80b3db2a475d1f9f9a1748f
RulH19 <nurulh298hd@gmail.com>
2025-01-02 13:59:15 +0700

    merapihkan struktur

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:masaku/views/splash_screen.dart';
+import 'package:masaku/views/auth-screen/splash_screen.dart';
 
 void main() {
   runApp(const MyApp());
